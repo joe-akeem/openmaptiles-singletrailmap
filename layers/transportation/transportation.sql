edfbb45a7be4cbb2a0642e752b778a7ad2eca78b
Admin <admin@h2914371.stratoserver.net>
2020-12-10 20:29:44 +0000

    Includ all paths already at Z12

--- layers/transportation/transportation.sql
+++ layers/transportation/transportation.sql
@@ -328,25 +328,7 @@ FROM (
                 z_order
          FROM osm_highway_linestring
          WHERE NOT is_area
-           AND (
-                     zoom_level = 12 AND (
-                             highway_class(highway, public_transport, construction) NOT IN ('track', 'path', 'minor')
-                         OR highway IN ('unclassified', 'residential')
-                     ) AND man_made <> 'pier'
-                 OR zoom_level = 13
-                         AND (
-                                    highway_class(highway, public_transport, construction) NOT IN ('track', 'path') AND
-                                    man_made <> 'pier'
-                            OR
-                                    man_made = 'pier' AND NOT ST_IsClosed(geometry)
-                        )
-                 OR zoom_level >= 14
-                         AND (
-                            man_made <> 'pier'
-                            OR
-                            NOT ST_IsClosed(geometry)
-                        )
-             )
+           AND zoom_level >= 12
          UNION ALL
 
          -- etldoc: osm_railway_linestring_gen_z8  ->  layer_transportation:z8
